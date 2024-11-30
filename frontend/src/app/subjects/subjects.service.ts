47f94ffcc45d744576317fd1856d3ace7e81afd8
Victor Kikuti <victor.kikuti@gmail.com>
2024-11-30 14:36:18 -0300

    add environments

--- frontend/src/app/subjects/subjects.service.ts
+++ frontend/src/app/subjects/subjects.service.ts
@@ -2,12 +2,14 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from './subjects';
 import { tap } from 'rxjs';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root',
 })
 export class SubjectsService {
-  private readonly API = 'http://localhost:8080/subjects';
+  //private readonly API = 'http://localhost:8080/subjects';
+  private readonly API = `${environment.API}subjects`;
 
   constructor(private http: HttpClient) {}
 
